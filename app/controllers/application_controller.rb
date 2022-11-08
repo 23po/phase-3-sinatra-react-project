675318ad986538ea6a9ddf92d0b8b3a6e708d34d
calvin <ckodalo@gmail.com>
2022-11-09 02:07:43 +0300

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < Sinatra::Base
    #set :default_content_type, 'application/json'
   
   # Add your routes here
-   get "/" do
-    '<h2>Hello <em>Jobs Api</em>!</h2>'
+   get '/' do
+    erb :index
    end
   
   get '/jobs' do
