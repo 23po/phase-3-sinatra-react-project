f72395c5a9643d15b60206e0232137ce2a4fc5bb
calvin <ckodalo@gmail.com>
2022-11-08 11:11:00 +0300

    first 2 migrations

---
+++ db/migrate/20221108072406_create_jobs.rb
@@ -0,0 +1,9 @@
+class CreateJobs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :organization
+      t.string :location
+      t.integer :user_id
+  end
+end
