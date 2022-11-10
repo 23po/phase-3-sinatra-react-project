963d9f8f98fc054faec613567fad75aba52c749f
calvin <ckodalo@gmail.com>
2022-11-10 16:52:13 +0300

    modify config file

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,13 @@
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
-# ENV['RACK_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= "development"
 set :database_file, “./database.yml”
 # Require in Gems
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['RACK_ENV'])
+require 'bundler/setup'
+Bundler.require(:default, ENV['RACK_ENV'])
 
-# # Require in all files in 'app' directory
-# require_all 'app'
+# Require in all files in 'app' directory
+set :database_file, "./database.yml"
+
+
+require_all 'app'
